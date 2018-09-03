80fef0af5e82aa9a39b4c8fefd9d37c3459055d5
Nelly Atuonwu <nellyatuonwu@gmail.com>
2018-09-03 02:49:57 +0100

    Squashed Conflicts

--- app/pages/help/help-page.js
+++ app/pages/help/help-page.js
@@ -1,8 +1,23 @@
-
-exports.onNavigatingTo = function(args) {
-  if (args.isBackNavigation) {
-    return;
+  const frameModule = require("ui/frame");
+  const HelpViewModel = require("./help-view-model");
+  
+  const helpViewModel = new HelpViewModel();
+  
+  exports.pageLoaded = function (args) {
+    const page = args.object;
+    page.bindingContext = helpViewModel;
   }
 
-  // do something
-};
+  
+  exports.showItems = function (args) {
+    const container = args.view;
+    const instructions = container.getChildAt(1);
+    const flexLayout = container.getChildAt(0);
+  
+    const arrow = flexLayout.getChildAt(1);
+  
+    instructions.visibility = instructions.bindingContext.collapsed ? 'visible' : 'collapse';
+    arrow.text = !instructions.bindingContext.collapsed ? '\uf107' : '\uf106';
+  
+    instructions.bindingContext.collapsed = !instructions.bindingContext.collapsed;
+  }
