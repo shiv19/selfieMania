0646ab8518303cd566a724e55cd637843a944cac
Shiva Prasad <sp@shiv19.com>
2018-09-06 06:44:28 +0530

    cleanup

--- app/main-page.js
+++ app/main-page.js
@@ -1,12 +1,3 @@
 const FirebaseServices = require('./shared/FirebaseService');
 const firebase = FirebaseServices.getInstance();
 firebase.init().then(() => {}).catch(e => {});
-
-let page;
-exports.pageLoaded = function(args) {
-  page = args.object;
-};
-
-exports.pageUnloaded = function(args) {
-  page = null;
-};
--- app/pages/explore/explore-page.js
+++ app/pages/explore/explore-page.js
@@ -1,7 +1,3 @@
-const appSettings = require('application-settings');
-// appSettings.setBoolean('isLoggedIn', false);
-// const firebase = require('nativescript-plugin-firebase');
-// firebase.logout().then(() => { console.log('logged out'); });
 exports.onNavigatingTo = function(args) {
   if (args.isBackNavigation) {
     return;
